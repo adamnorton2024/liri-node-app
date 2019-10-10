57c452da8e70a47c49e1a618dd5ae4e2458b6949
Adam Norton <adam@adamnorton.com>
2019-10-10 17:31:10 -0500

    movie and concert functions work. Need to get solution for when Rotten Tomatoes review is missing.

--- liri.js
+++ liri.js
@@ -36,7 +36,31 @@ switch (command) {
 
 function concert(artist){
     console.log("Get Concert Data");
-};
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(queryUrl).then(
+        function (response) {
+
+            //console.clear();
+            // artist = artist.replace(/+/g, ' ');
+            console.log(response.data);
+            for (let i = 0; i < response.data.length; i++) {
+                console.log("=================================");
+                console.log(" ");
+                console.log("Lineup: " + response.data[i].lineup);
+                console.log("Venue: " + response.data[i].venue.name);
+                console.log("Location: " + response.data[i].venue.city, response.data[i].venue.region);
+                console.log("Date / Time: " + moment(response.data[i].datetime).format("MM-DD-YYYY, hh:mm"));
+                console.log(" ");
+            };
+        })
+
+        .catch(function (error) {
+            if (error.response) {
+            console.log(error.message);
+            };
+        }
+    )};
 
 function spotifyThis(song){
     console.log("Get Spotify Data");
@@ -44,13 +68,24 @@ function spotifyThis(song){
 
 function movie(title){
     console.log("Get Movie Data");
-    var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy"
+    var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
 
     axios.get(queryUrl).then(
         function (response) {
+            console.clear();
             console.log("===========================================");
             console.log("Title: " + response.data.Title);
             console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            if (!JSON.stringify(response.data.Ratings[1].Value)){
+                console.log("Rotten Tomatoes Rating: N/A");
+            } else {
+                console.log("Rotten Tomatoes Rating: " + JSON.stringify(response.data.Ratings[1].Value));
+            };
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
         })
         .catch(function (error) {
             if (error.response) {
