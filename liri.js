9fd5fb5fea4c8ca0ec1fb0afd905a42f4757ed7e
Adam Norton <adam@adamnorton.com>
2019-10-14 20:00:30 -0500

    made the random function work which uses the commands specified in random.txt

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+// setup all the required external links
 require("dotenv").config();
 var fs = require("fs");
 var axios = require("axios");
@@ -7,50 +8,57 @@ Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
+// grab command line arguments and save them as variables. 
 var nodeArg = process.argv;
 var name = nodeArg.slice(3).join("+");
 //console.log(name);
 var command = nodeArg[2];
 //console.log(command);
 
-switch (command) {
-    case "concert-this":
-        concert(name);
-        break;
-    case "spotify-this-song":
-        if(!name){
-            name = "Ace of Base The Sign US Album [Remastered]";
-        }
-        spotifyThis(name);
-        break;
-    case "movie-this":
-        if(!name){
-            name = "Mr. Nobody";
-        }
-        movie(name);
-        break;
-    case "do-what-it-says":
-        random();
-        break;
-    default:
-        console.log("User Error! Must use one of the following commands:");
-        console.log('"concert-this"');
-        console.log('"spotify-this-song"');
-        console.log('"movie-this"');
-        console.log('"do-what-it-says"');
-        break;
-}
+liriControl(); // call function that decides which command to execute by calling the correct funtion
+
+function liriControl(){
+    switch (command) {
+        case "concert-this":
+            concert(name);
+            break;
+        case "spotify-this-song":
+            if (!name) {
+                name = "Ace of Base The Sign US Album [Remastered]";
+            }
+            spotifyThis(name);
+            break;
+        case "movie-this":
+
+            // if no movie name was given, then use Mr. Nobody.
+            if (!name) {
+                name = "Mr. Nobody";
+            }
+            movie(name);
+            break;
+        case "do-what-it-says":
+            random();
+            break;
+        default:
+            console.log("User Error! Must use one of the following commands:");
+            console.log('"concert-this"');
+            console.log('"spotify-this-song"');
+            console.log('"movie-this"');
+            console.log('"do-what-it-says"');
+            break;
+    }
+};
+
+
+
 
 function concert(artist){
-    //console.log("Get Concert Data");
+    // setup query for requested concert data
     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
     axios.get(queryUrl).then(
         function (response) {
-
-            //console.clear();
-            // artist = artist.replace(/+/g, ' ');
-           // console.log(response.data);
+            // loop through all the responses to list out all upcoming concerts
             for (let i = 0; i < response.data.length; i++) {
                 console.log("=================================");
                 console.log(" ");
@@ -70,15 +78,17 @@ function concert(artist){
     )};
 
 function spotifyThis(song){
-    console.log("Get Spotify Data");
-    console.log("Song is: " + song);
-
+   //console.log("Get Spotify Data");
+    //console.log("Song is: " + song);
+        // call for spotify song by name
         spotify
             .search({ type: 'track', query: song })
             .then(function (response) {
 
-
+                
                 var songData = response.tracks.items;
+
+                // case of if they didn't specify a song.
                 if (song === "Ace of Base The Sign US Album [Remastered]"){
                     console.log("Should only be one song");
                     console.log("===========================================")
@@ -89,7 +99,7 @@ function spotifyThis(song){
                     console.log("Preview Link: " + songData[0].preview_url);
                     console.log(" ");
                 } else {
-
+                    // list all songs with same or similar name
                     for (let i = 0; i < songData.length; i++) {
                         console.log("===========================================")
                         console.log(" ");
@@ -110,10 +120,11 @@ function spotifyThis(song){
 };
 
 function movie(title){
-    console.log("Get Movie Data");
+    //console.log("Get Movie Data");
     var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
-
+    // request information based on movie title given
     axios.get(queryUrl).then(
+        // list movie info
         function (response) {
             console.clear();
             console.log("===========================================");
@@ -153,17 +164,23 @@ function movie(title){
 };
 
 function random(){
-    console.log("Do Random Stuff");
+    //console.log("Do Random Stuff");
+    // if this function is run, it'll read the random.txt file for what to use as the commands.
     fs.readFile("random.txt", "utf8", function(error, data) {
 
         if(error){
             return console.log(error);
         };
 
-        console.log(data);
-
+        //console.log(data);
+        // saving data from random.txt, splitting it at the comma, and saving into an array. 
         var dataArr = data.split(",");
 
-        console.log(dataArr);
+        // change these variables to match data saved into dataArr.
+        command = dataArr[0];
+        name = dataArr[1];
+
+        // run the switch statement again to retrieve the wanted info. 
+        liriControl();
     });
 };
